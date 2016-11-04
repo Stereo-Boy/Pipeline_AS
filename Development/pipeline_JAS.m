57cd782c93946d7556b96b7de3018ce48fd97f57
Justin Theiss <jdeantheiss@gmail.com>
2016-11-04 15:50:27 -0700

    Updated pipeline_JAS to allow simpler representation of steps.

--- Development/pipeline_JAS.m
+++ Development/pipeline_JAS.m
@@ -5,6 +5,33 @@ function pipeline_JAS(stepList2run)
 % run
 % If stepList2run is not defined, it will show a menu of all steps and asks
 % for an answer.
+%
+% Steps available to run:
+%   0. All of the below steps
+%   1. mprage: nifti conversion
+%   2. mprage: fix nifti header
+%   3. mprage: segmentation using FSL
+%   4. mprage: correction of gray mesh irregularities
+%   5. retino epi: nifti conversion and removal of ''pRF dummy'' frames
+%   6. retino epi: motion correction and MC parameter check
+%   7. retino epi: artefact removal
+%   8. retino epi: fix nifti headers
+%   9. retino epi: initialization of mrVista session
+%   10. retino epi: alignment of inplane and volume
+%   11. retino epi: segmentation installation
+%   12. retino epi: pRF model
+%   13. retino epi: mesh visualization of pRF values
+%   14. retino epi: extraction of flat projections
+%   15. exp epi: nifti conversion
+%   16. exp epi: motion correction and MC parameter check
+%   17. exp epi: artefact removal
+%   18. exp epi: fix nifti headers
+%   19. exp epi: initialization of mrVista session
+%   20. exp epi: alignment of inplane and volume
+%   21. exp epi: segmentation installation
+%   22. exp epi: GLM sanity check
+%   23. exp epi: actual GLM model
+%   24. exp epi: mesh visualization
 % ------------------------------------------------------------------------
 % Written Nov 2016
 % Justin Theiss and Adrien Chopin
@@ -16,32 +43,7 @@ function pipeline_JAS(stepList2run)
 
 % MENU
 if exist('stepList2run', 'var')==0
-    dispi('What step do you want to run?')
-    dispi('0. All of the below steps')
-    dispi('1. mprage: nifti conversion')
-    dispi('2. mprage: fix nifti header')
-    dispi('3. mprage: segmentation using FSL')
-    dispi('4. mprage: correction of gray mesh irregularities')
-    dispi('5. retino epi: nifti conversion and removal of ''pRF dummy'' frames')
-    dispi('6. retino epi: motion correction and MC parameter check')
-    dispi('7. retino epi: artefact removal')
-    dispi('8. retino epi: fix nifti headers')
-    dispi('9. retino epi: initialization of mrVista session')
-    dispi('10. retino epi: alignment of inplane and volume')
-    dispi('11. retino epi: segmentation installation')
-    dispi('12. retino epi: pRF model')
-    dispi('13. retino epi: mesh visualization of pRF values')
-    dispi('14. retino epi: extraction of flat projections')
-    dispi('15. exp epi: nifti conversion')
-    dispi('16. exp epi: motion correction and MC parameter check')
-    dispi('17. exp epi: artefact removal')
-    dispi('18. exp epi: fix nifti headers')
-    dispi('19. exp epi: initialization of mrVista session')
-    dispi('20. exp epi: alignment of inplane and volume')
-    dispi('21. exp epi: segmentation installation')
-    dispi('22. exp epi: GLM sanity check')
-    dispi('23. exp epi: actual GLM model')
-    dispi('24. exp epi: mesh visualization')
+    help(mfilename);
     stepList2run=input('Enter the numbers of the desired steps, potentially between brackets: ');
 end
 
