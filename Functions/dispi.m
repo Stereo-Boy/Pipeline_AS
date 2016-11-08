7b751e82061adb1f67bb34b27877e7d476131d9a
Justin Theiss <jdeantheiss@gmail.com>
2016-11-08 09:11:45 -0800

    Modified dispi to allow for line returns, tabs, etc.

--- Functions/dispi.m
+++ Functions/dispi.m
@@ -2,26 +2,38 @@ function dispi(varargin)
 % Format-proof dispi function with verbose option
 % This function just disp anything, even numbers. 
 % You can also just provide all the message parts separated by commas rather
-% that producing a string between brackets.
+% than producing a string between brackets.
 % If dispi is provided with an argument 'verboseOFF' then it just skips that function.
 % 'verboseON' is simply ignored.
-% Adrien Chopin, nov 2016
+% 
+% Example:
+% 
+% dispi('Sometimes it''s nice to separate\n',4,'\ndifferent\nlines!');
+%
+% Sometimes it's nice to separate
+% 4
+% different
+% lines!
+%
+% dispi('Sometimes I don''t want to display anything','verboseOFF');
+%
+% Created by Adrien Chopin, nov 2016
 
-nbArg=numel(varargin);
-if nbArg>0
-    string=[];
-    for i=1:nbArg
-        if strcmpi(varargin{i},'verboseOFF')
-           return 
-        elseif strcmpi(varargin{i},'verboseON')
-            %do nothing
-        else
-            string=[string,num2str(varargin{i})];
-        end
-    end
-    disp(string)
-else
-        disp('No arguments provided to dispi for display...')
-end
+% check for verboseOFF
+if any(strcmpi(varargin,'verboseOFF')), return; end;
 
+% remove verboseON
+varargin(strcmpi(varargin,'verboseON')) = [];
+
+% for each argument, display
+string=[];
+for i=1:numel(varargin)
+    % concatenate string
+    string=[string,num2str(varargin{i})];
 end
+% add return carriage for fprintf
+string=[string,'\n'];
+
+% display string (using fprintf to allow for returns etc.)
+fprintf(string);
+return;
