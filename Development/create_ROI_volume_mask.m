9c5da0d053861c370fc9aaa80883ca9b597eab80
Adrien_Chopin <adrien.chopin@gmail.com>
2019-08-09 10:58:24 +0200

    new mvpa decoding file with volume mask creation file

--- Development/create_ROI_volume_mask.m
+++ Development/create_ROI_volume_mask.m
@@ -1,4 +1,4 @@
-function maskNii = create_ROI_volume_mask(volumePath, roiPath)
+function [maskNii,n_vox] = create_ROI_volume_mask(volumePath, roiPath)
 % ------------------------------------------------------------------------
 % Take a nifti volume file and a ROI.mat file and create a spm-like 
 % volume mask restricted to the ROI
@@ -20,26 +20,32 @@ function maskNii = create_ROI_volume_mask(volumePath, roiPath)
     
 % retrieve dimensions of volume file
     dims = maskNii.dim;
+
 % set mask data to 0's
-    maskNii.data = int8(zeros(dims(1), dims(2), dims(3)));
+    %masknii.data = int8(zeros(dims(1), dims(2), dims(3)));
+    data = int8(zeros(dims(1), dims(2), dims(3)));
     
 % read ROI file and change corresponding data values in mask to 1
     load(roiPath);
-    
+
     for i = 1:3:numel(ROI.coords)
        x = ROI.coords(i); % x is Ant>Post
        y = ROI.coords(i+1); % y is Left->Right
        z = ROI.coords(i+2); % z is Inf->Sup
-       maskNii.data(z, dims(2) - y, dims(3) - x) = int8(1);
+       data(z, dims(2) - y, dims(3) - x) = int8(1);
     end
-    
+    n_vox=sum(data(:));
 % save mask in file
     %p  = fileparts(volumePath);
     %maskPath = fullfile(p,'ROI_volume_mask.nii');
-    [path_roi,roiMask]=fileparts(roiPath);
-    maskNii.fname=fullfile(path_roi,['ROI_',roiMask,'.nii']);
-    spm_write_vol(maskNii,maskNii.data);
+    [path_roi,roi_name]=fileparts(roiPath);
+    maskNii.fname=fullfile(path_roi,['ROI_',roi_name,'.nii']);
+    %maskPath=fullfile(path_roi,['ROI_',roi_name,'.nii']);
+    spm_write_vol(maskNii,data);
     %niftiWrite(maskNii, maskPath); 
     %save_nii(maskNii, maskNii.fname); 
-
+    
+    %maskNii = spm_vol_nifti(maskPath);
+    %dat=spm_read_vols(maskNii);
+    %spm_write_vol(maskNii,dat);
 end
